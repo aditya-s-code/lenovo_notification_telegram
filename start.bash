783f8b0e1f371e4dbf38febda5c0ab6e639dcbb9
aditya-s-code <mr.aditya1446@gmail.com>
2025-06-28 22:00:15 +0530

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Starting your Lenovo Notification Telegram app..."
-python main.py
+echo "Starting your Lenovo Notification Telegram app with Gunicorn..."
+gunicorn main:app --bind 0.0.0.0:$PORT
 echo "Application has stopped."
